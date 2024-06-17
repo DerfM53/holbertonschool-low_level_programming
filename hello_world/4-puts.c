e35470509f4bc8a8e4addb44003764502cf6f378
DerfM53 <frederic.mieuzet@gmail.com>
2024-06-17 13:58:29 +0000

    modfile

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-
+/**
+* main - Entry point*
+*
+* Description: 'the program's print text'
+*
+* Return: Always 0 (Success)
+*
+*/
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-
