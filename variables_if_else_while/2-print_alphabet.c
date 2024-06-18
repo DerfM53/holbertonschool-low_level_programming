31bdd379d873f3ae7d548fad18675241d0b6718e
DerfM53 <frederic.mieuzet@gmail.com>
2024-06-18 17:16:52 +0000

    recordfile

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description: programm affiche toute les lettres de l'alphabet
+*en minuscule et en majuscule avec un retour a la ligne
+*
+*Return: Always 0 (Success)
+*/
+int char main(void);
+{
+while
+{
+(a <= z) a++;
+}
+putchar(a);
+while
+{
+(A <= Z) A++;
+}
+putchar(A);
+putchar('\n');
+return (0);
+}
