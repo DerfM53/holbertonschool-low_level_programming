d2fd8a618f0cde35ea1c092980774857e2417197
DerfM53 <frederic.mieuzet@gmail.com>
2024-06-18 13:05:04 +0000

    adfile

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/*
+* determine et affiche si le nombre est e√gale
+* superieur et inferieure a zero
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else if
+(n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is positive\n", n);
+}
+return (0);
+}
