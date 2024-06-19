1cbd6947a8cabda1f7e95f2974069ca40a5b8356
DerfM53 <frederic.mieuzet@gmail.com>
2024-06-19 08:40:54 +0000

    modfile

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description: programm affiche toute les lettres de l'alphabet
+*en minuscule
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int c;
+for (c = 0; c <= 9; ++c)
+{
+printf("%d", c);
+}
+printf("\n");
+return (0);
+}
