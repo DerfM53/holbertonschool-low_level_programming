9095e89622f45f87260d9d3243c23d15531543a7
DerfM53 <frederic.mieuzet@gmail.com>
2024-06-20 12:45:22 +0000

    addfile

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - point d'entrée du programme
+ *
+ * Return: 0 si le programme s'exécute avec succès
+ */
+int main(void)
+{
+_putchar(); /* Appelez la fonction _putchar */
+return (0);
+}
+
---
+++ functions_nested_loops/_putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - affiche un ensemble de lettres
+ *
+ * Return: 0 si le programme s'exécute avec succès
+ */
+int _putchar(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
