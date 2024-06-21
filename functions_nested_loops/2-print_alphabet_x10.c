c78cb7c3a009170d401819aedc9f0c946aee3d0a
DerfM53 <frederic.mieuzet@gmail.com>
2024-06-21 12:43:45 +0000

    modfile

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -15,10 +15,14 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char letter;
 
 	for (i = 0; i < 10; i++)
-{
-	_putchar(i);
-}
-_putchar('\n');
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+		_putchar(letter);
+		}
+		_putchar('\n');
+	}
 }
