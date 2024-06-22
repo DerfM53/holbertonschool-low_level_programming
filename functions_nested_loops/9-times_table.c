b836bfaae4f953a9f15ecfd49b9482a805c48464
DerfM53 <frederic.mieuzet@gmail.com>
2024-06-22 13:43:58 +0000

    modfile

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - affiche la table des * jusqu'a
+ * fois 9.
+ *
+ * _putchar affiche le resultat
+ *
+ */
+
+void times_table(void)
+{
+	int i, j, product;
+
+	for (i = 0; i <= 9; i++)
+{
+	for (j = 0; j <= 9; j++)
+	{
+	product = i * j;
+
+	if (i > 0)
+	{
+		_putchar(',');
+		_putchar(' ');
+		if (product < 10)
+		_putchar(' ');
+	}
+	_putchar('0' + product);
+	}
+	_putchar('\n');
+	}
+}
