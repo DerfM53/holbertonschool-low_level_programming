ad0a80490e7ef2e34cec1f5f800a3a2d95154575
DerfM53 <frederic.mieuzet@gmail.com>
2024-06-23 10:28:02 +0000

    modfile

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -18,14 +18,14 @@ void times_table(void)
 	{
 	product = i * j;
 
-	if (product < 10)
+	if (product <= 9)
 	{
 		_putchar('0' + product);
 		_putchar(',');
 		_putchar(' ');
 		_putchar(' ');
 	}
-	else if (product > 10)
+	else if (product >= 9)
 	{
 	_putchar((product / 10) + '0');
 	_putchar((product % 10) + '0');
