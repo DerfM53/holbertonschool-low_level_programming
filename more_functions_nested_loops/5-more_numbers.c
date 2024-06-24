b45cecc52ad39fdae4b881c1c58a4c61f0f3fd9b
DerfM53 <frederic.mieuzet@gmail.com>
2024-06-24 11:57:14 +0000

    modfile

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * more_numbers - imprime les chiifres de 0 a 14
+ * sur 10 lignes
+ *
+ * n : le chiffres unitaires affich√
+ * ligne : le nombre de ligne©
+ */
+
+void more_numbers(void)
+{
+int n;
+int ligne;
+
+for (ligne = 0; ligne < 10; ligne++)
+{
+	for (n = 0; n < 15; n++)
+	{
+	 if (n > 9)
+	{
+	 _putchar(n / 10 + '0');
+	}
+	_putchar(n % 10 + '0');
+	}
+	_putchar('\n');
+}
+}
