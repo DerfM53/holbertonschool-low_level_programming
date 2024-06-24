06cb6deb4994b0d45d12517b40dec213d6c66ce0
DerfM53 <frederic.mieuzet@gmail.com>
2024-06-24 13:59:33 +0000

    modfile

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,13 @@
  * print_numbers - affiche les chiffres de 0 a 9.
  *
  */
-
 void print_numbers(void)
 {
 	char c;
 
-	 for (c = 0; c <= 9; c++)
-{
-	_putchar(c + '0');
-}
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
 	_putchar('\n');
 }
