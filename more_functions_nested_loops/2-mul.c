309b0d71b4bfa105f6c1f8e9af113616cfde37ae
DerfM53 <frederic.mieuzet@gmail.com>
2024-06-24 08:44:57 +0000

    modfile

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplie a et b.
+ *
+ * @a: valeur
+ * @b: valeur
+ *
+ * return : la multiplication de a et b
+ *
+ */
+
+int mul(int a, int b)
+
+	{
+	return (a * b);
+	}
