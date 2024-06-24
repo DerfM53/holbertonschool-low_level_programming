5f5701b2c240d928225e90db00a90488e4232aa5
DerfM53 <frederic.mieuzet@gmail.com>
2024-06-24 14:02:50 +0000

    modfile

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,18 +2,13 @@
 /**
  * print_most_numbers - imprime le caract√re de0 a 9
  * sauf le 2 et le 4
- *
- * _putchar: imprime de resultat
- *
- * @c: variable d√terminer
- *
  */
 
 void print_most_numbers(void)
 {
 	char c;
 
-	 for (c = 0; c <= 9; c++)
+	for (c = 0; c <= 9; c++)
 {
 	if (c != 2 && c != 4)
 {
