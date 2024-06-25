88ffb63436c32a3c2dbd8381163b42601f2a0619
DerfM53 <frederic.mieuzet@gmail.com>
2024-06-25 06:19:04 +0000

    modfile

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,25 @@
 #include "main.h"
-/**
- *
- */
 
+/**
+ * print_line - Dessine une ligne dans le terminal
+ * @n: Le nombre de fois que le caractère _ doit être imprimé
+ *
+ * Return: void
+ */
 void print_line(int n)
 {
-if (n <= 0)
+	int i;
 
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (i = 0; i < n; i++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+	}
+}
