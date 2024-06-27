40849c2bdefc51b41b8c8aa5bbbe110e09fa0d9f
DerfM53 <frederic.mieuzet@gmail.com>
2024-06-27 12:42:18 +0000

    modfile

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_rev - Affiche une chaÃ®ne de caractÃ¨ dans l'ordre inversÃes
+ * rev_string - Affiche une chaÃ®ne de caractÃ¨ dans l'ordre inversÃes
  * et retour a la ligne
  *
  * @s: Pointeur vers la chaÃ®ne de caractre.Ã
