63c3fd5ea8e135adc57fb1936a260b23d48bd170
DerfM53 <frederic.mieuzet@gmail.com>
2024-06-28 08:04:51 +0000

    modfile

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -4,20 +4,20 @@
  * print_array - affiche les entiers des ÃÃlÃments d'un tabau
  *
  * @a: pointeur vers tableau
- * @n: nombre d'ÃlÃments du table
+ * @n: nombre d'eÃeÃments du tab
  *
  */
 void print_array(int *a, int n)
 {
 for (int i = 0; i < n; i++)
 	{
-	if (i <= -1)
+	if (i <= 3)
 		{
-		printf("%d", a[i]);
+		printf("%d, ", a[i]);
 		}
 	else
 		{
-		printf("%d ,", a[i]);
+		printf("%d", a[i]);
 		}
 	}
 printf("\n");
