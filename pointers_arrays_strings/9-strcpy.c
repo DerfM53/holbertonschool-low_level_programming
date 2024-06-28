6a23e4466a59e53950a4062b4cae04a547dc54f1
DerfM53 <frederic.mieuzet@gmail.com>
2024-06-28 09:14:02 +0000

    modfile

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strcpy - copie une chaine de caractere pour la mettre
+ *  dans une autre.
+ * Return: retourne le contenu de dest
+ * @dest: pointeur return:
+ * @src: pointeur arrivÃ
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+while (src[i] != '\0')
+	{
+	dest[i] = src[i];
+	i++;
+	}
+return (dest);
+}
