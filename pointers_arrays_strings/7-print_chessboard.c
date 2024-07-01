0ea6b5295e82c2a4789ae7a8b0bd422706bb255e
DerfM53 <frederic.mieuzet@gmail.com>
2024-07-01 21:15:29 +0000

    modfile

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_chessboard - Affiche un échiquier représenté par un tableau 8x8.
+ * @a: Pointeur vers le tableau 2D représentant l'échiquier.
+ *
+ * Description :
+ * Cette fonction parcourt un tableau 2D représentant un échiquier et affiche
+ * chaque caractère sur la sortie standard, suivis d'un reto la ligne aprè
+ * chaque ligne complète.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+for (i = 0; i < 8; i++)
+	{
+	for (j = 0; j < 8; j++)
+		{
+		if (j == 7)
+		{
+		_putchar('\n');
+		}
+		else
+		{
+		_putchar(a[i][j]);
+		}
+		}
+	}
+
+}
