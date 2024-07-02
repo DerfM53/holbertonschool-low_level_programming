cfa66b051a388e733ac769c191e2d6bd58319681
DerfM53 <frederic.mieuzet@gmail.com>
2024-07-02 20:18:30 +0000

    modfile

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,9 +1,16 @@
 #include "main.h"
 /**
- *
- *
- *
+ * _puts_recersion - fonction qui imprime une chaine
+ * de caract√res puis un retour a la ligne.
  *
  */
 void _puts_recursion(char *s)
-
+{
+if (*s == '\0')
+	{
+	_putchar('\n');
+	return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
