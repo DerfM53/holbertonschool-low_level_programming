27b81ff8b139e8e1a2dbe756ea2aae738ac15bdd
DerfM53 <frederic.mieuzet@gmail.com>
2024-07-04 13:41:18 +0000

    modfile

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ *
+ */
+int main(int argc, char **argv)
+{
+	if (argc == 3)
+	{
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
+	return (0);
+	}
+	else
+	{
+	printf("ERREUR\n");
+	return (1);
+	}
+}
