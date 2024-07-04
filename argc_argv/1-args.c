4dbf69b352b3ccb9edcf270c14e0023dc770df80
DerfM53 <frederic.mieuzet@gmail.com>
2024-07-04 12:39:00 +0000

    modfile

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
+ * main - imprime le nombre d'arguments passé programm
+ * @argc: nombre d'arguments passés au programme
+ * @argv: tableau des arguments passés au programme (non utilisé)
  *
- *
- *
- *
- *
- *
+ * Description: Ce programme affiche le nombre d'arguments
+ *              en excluant le nom du programme lui-même.
+ * Return: Toujours 0 (succès)
  */
+int main(int argc, char **argv)
+{
+	(void)argv;
 
+	printf("%d\n", argc - 1);
+	return (0);
+}
