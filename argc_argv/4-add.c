10a0801c65fdf46b911065df0872ad78a7df9836
DerfM53 <frederic.mieuzet@gmail.com>
2024-07-05 08:54:11 +0000

    modfile

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
-/*
+#include <stdlib.h>
+/**
+ *main - Additionne les entiers passés en arguments
+ * @argc: Nombre d'arguments
+ * @argv: Tableau de chaînes de caractères représentant les arguments
  *
+ * Description: Ce programme additionne les entiers passés en arguments
+ * et affiche le résultat. Si aucun argument n'est passé
+ * il affiche une erreur
+ *
+ * Return: 0 si l'addition est réussie, 1 si une erreur se produit
  *
  */
 
 int main(int argc, char **argv)
 {
-	if (argc == 3)
-	{
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
+	int somme = 0;
+	int i;
 
-	printf("%d\n", num1 * num2);
-	return (0);
-	}
-	else
+if (argc < 3)
 	{
-	printf("ERREUR\n");
+	printf("Error\n");
 	return (1);
 	}
+for (i = 1; i < argc; i++)
+	{
+	somme += atoi(argv[i]);
+	}
+	printf("%d\n", somme);
+	return (0);
 }
