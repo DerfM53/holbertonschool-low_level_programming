b4ef6942abbf614ecd9e00c651a940a5a173b59f
DerfM53 <frederic.mieuzet@gmail.com>
2024-07-05 07:49:34 +0000

    modfile

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ *  main - Multiplie deux entiers passés en arguments
+ * @argc: Nombre d'arguments
+ * @argv: Tableau de chaînes de caractères représentant les arguments
  *
+ * Description: Ce programme multiplie deux entiers passés en arguments
+ * et affiche le résultat. Si le nombre d'arguments est incorrect,
+ * il affiche une erreur.
  *
- *
+ * Return: 0 si la multiplication est réussie
+ * 1 si une erreur se produit
  *
  */
 int main(int argc, char **argv)
 {
 	if (argc == 3)
 	{
-int num1 = atoi(argv[1]);
-int num2 = atoi(argv[2]);
+
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
 
 	printf("%d\n", num1 * num2);
 	return (0);
 	}
 	else
 	{
-	printf("ERREUR\n");
+	printf("Error\n");
 	return (1);
 	}
 }
