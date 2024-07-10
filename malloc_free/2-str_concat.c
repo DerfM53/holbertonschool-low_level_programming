16bf82f05666eefba8802771fb4e769e3b5d4dcc
DerfM53 <frederic.mieuzet@gmail.com>
2024-07-10 08:43:06 +0000

    addfile

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,32 +1,42 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 /**
- *str_concat - Concatène deux chaînes de cara
- * @s1: Première chaî
- * @s2: Deuxième chaîne à concaténer à
+ * str_concat - Concatène deux chaînes de caractères
+ * @s1: Première chaîne
+ * @s2: Deuxième chaîne
  *
- * Return: Pointeur vers la nouvelle chaîne d
- * caractères concatén
- * NULL si l'allocation de mémoire écue
+ * Return: Pointeur vers la nouvelle chaîne de caractères concaténée
+ * NULL si l'allocation de mémoire échoue
  */
 char *str_concat(char *s1, char *s2)
 {
-size_t len1 = strlen(s1);
-size_t len2 = strlen(s2);
-char *NewString = malloc(len1 + len2 + 1);
+	char *new_str;
+	int len1 = 0, len2 = 0, i, j;
 
 	if (s1 == NULL)
-		s1 = "";
+	s1 = "";
 	if (s2 == NULL)
-		s2 = "";
+	s2 = "";
 
-	if (NewString == NULL)
-		return (NULL);
+	while (s1[len1] != '\0')
+	len1++;
 
-	strcpy(NewString, s1);
-	strcat(NewString, s2);
+	while (s2[len2] != '\0')
+	len2++;
 
-return (NewString);
+	new_str = malloc((len1 + len2 + 1) * sizeof(char));
+	if (new_str == NULL)
+	return (NULL);
+
+	for (i = 0; i < len1; i++)
+	new_str[i] = s1[i];
+
+	for (j = 0; j < len2; j++, i++)
+	new_str[i] = s2[j];
+
+	new_str[i] = '\0';
+
+	return (new_str);
 }
