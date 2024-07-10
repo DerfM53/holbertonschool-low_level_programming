1cf5f1a1e2c84d9d2c38c620dbd49b6cd013f48a
DerfM53 <frederic.mieuzet@gmail.com>
2024-07-10 09:45:46 +0000

    modfile

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *malloc_checked - Alloue de la mémoire en utilisant malloc.
+ * @b: Nombre d'octets à allouer.
+ *
+ * Return: Pointeur vers la méoire allouée
+ * Termine le programme avec le statut 98 en cas d'échec de malloc
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *memory = malloc(b * sizeof(int));
+
+	if (b <= 0)
+	{
+	exit(98);
+	}
+	return (memory);
+}
