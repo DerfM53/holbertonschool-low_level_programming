4961194f02debe4be0f2863e0a8b76bd5db6c4c9
DerfM53 <frederic.mieuzet@gmail.com>
2024-07-10 07:46:50 +0000

    modfile

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,33 +1,34 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
- * _strdup - Duplicate a string using malloc
- * @str: The string to duplicate
+ * _strdup - Retourne un pointeur vers un nouvel espace alloué en mémoire,
+ * qui contient une copie de la chaîne donnée en paramètre.
+ * @str: La chaîne à dupliquer.
  *
- * Return: Pointer to the newly allocated duplicate string,
- * or NULL if str is NULL or if malloc fails
+ * Return: Un pointeur vers la chaîne dupliquée, ou NULL si str est NULL
+ * ou si une mémoire insuffisante était disponible.
  */
 char *_strdup(char *str)
 {
-	char *dup;
-	int c;
+	char *duplicate;
+	unsigned int i, length;
 
-if (str == NULL)
+	if (str == NULL)
 	{
-	return (NULL);
+		return (NULL);
 	}
-
-	dup = malloc(strlen(str) + 1);
-if (dup == NULL)
+	for (length = 0; str[length] != '\0'; length++)
 	{
-	return (NULL);
 	}
-for (c = 0 ; str[c] != '\0'; c++)
+		duplicate = malloc((length + 1) * sizeof(char));
+	if (duplicate == NULL)
 	{
-	dup[c] = str[c];
+		return (NULL);
 	}
-	dup[c] = '\0';
-return (dup);
+	for (i = 0; i < length; i++)
+	{
+		duplicate[i] = str[i];
+	}
+		duplicate[length] = '\0';
+	return (duplicate);
 }
