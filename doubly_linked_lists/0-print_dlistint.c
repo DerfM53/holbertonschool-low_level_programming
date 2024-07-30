807dff700c75d60c7a6010c1a4a5d43af8ae977a
DerfM53 <frederic.mieuzet@gmail.com>
2024-07-30 22:37:58 +0200

    create and complete file for project 1

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+* print_dlistint: print all the elements of a
+* dlistint_t list.
+* @h: pointer to the head of the list
+* Return: number of nodes
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t nbNode = 0;
+    const dlistint_t *actual = h;
+
+    while (actual != NULL)
+    {
+        printf("%d\n", actual->n);
+
+    nbNode++;
+    actual = actual->next;
+    }
+return (nbNode);
+}
