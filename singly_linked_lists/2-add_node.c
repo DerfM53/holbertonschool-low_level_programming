cb214a0d8f18caccee6e815f218f476140a61bb2
DerfM53 <frederic.mieuzet@gmail.com>
2024-07-31 19:46:30 +0000

    correction syntaxe files for betty

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,7 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <string.h>
-
 /**
  * add_node - Adds a new node at the beginning of a list_t list.
  * @head: Double pointer to the head of the list.
@@ -11,30 +10,30 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *newnode;
+	list_t *newnode;
 
-    newnode = malloc(sizeof(list_t));
-    if (newnode == NULL) /*Check if malloc failed*/
-    {
-        return (NULL);
-    }
+	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL) /*Check if malloc failed*/
+	{
+	return (NULL);
+	}
 
-    /*Duplicate the string and assign it to the new node*/
-    newnode->str = strdup(str);
-    if (newnode->str == NULL) /*Check if strdup failed*/
-    {
-        free(newnode); /*Free the allocated node if strdup fails*/
-        return (NULL);
-    }
+	/*Duplicate the string and assign it to the new node*/
+	newnode->str = strdup(str);
+	if (newnode->str == NULL) /*Check if strdup failed*/
+	{
+	free(newnode); /*Free the allocated node if strdup fails*/
+	return (NULL);
+	}
 
-    /*Calculate the length of the string and assign it to the new node*/
-    newnode->len = strlen(newnode->str);
-    
-    /*Make the new node point to the current head*/
-    newnode->next = *head;
+	/*Calculate the length of the string and assign it to the new node*/
+	newnode->len = strlen(newnode->str);
 
-    /*Update the head to point to the new node*/
-    *head = newnode;
+	/*Make the new node point to the current head*/
+	newnode->next = *head;
 
-    return (newnode); /*Return the address of the new node*/
+	/*Update the head to point to the new node*/
+	*head = newnode;
+
+	return (newnode); /*Return the address of the new node*/
 }
