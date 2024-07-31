220c7131c8474fa2018b29c0ad1d32a0d161700e
DerfM53 <frederic.mieuzet@gmail.com>
2024-07-31 19:42:21 +0000

    correction syntaxe files for betty

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -3,12 +3,13 @@
  * list_len - Returns the number of elements in a linked list
  * @h: Pointer to the head of the list
  *
- * Return: Number of elements in the list*/
+ * Return: Number of elements in the list
+ */
 size_t list_len(const list_t *h)
 {
-	size_t len = 0; /*compteur pour le nombre de noeud*/	
+	size_t len = 0; /*compteur pour le nombre de noeud*/
 	const list_t *actual = h; /*pointeur pour parcourir la liste*/
-	
+
 	while (actual != NULL) /*tand que le noeud actuel n'est pas NULL*/
 	{
 		if (actual->str == NULL)/*si la chaine est NULL*/
