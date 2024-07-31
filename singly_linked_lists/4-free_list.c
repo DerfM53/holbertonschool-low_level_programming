28f0b6c79897f79b350fbe49eec24b96ca873c71
DerfM53 <frederic.mieuzet@gmail.com>
2024-07-31 19:56:39 +0000

    correction syntaxe files for betty

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,22 +1,23 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
-* free_list - Frees a list_t list.
-* @head: Pointer to the head of the list to be freed.
-*
-* Description: This function traverses the linked list and frees each
-* node one by one. After freeing all nodes, it sets the head pointer
-* to NULL.
-*/
+ * free_list - Frees a list_t list.
+ * @head: Pointer to the head of the list to be freed.
+ *
+ * Description: This function traverses the linked list and frees each
+ * node one by one. After freeing all nodes, it sets the head pointer
+ * to NULL.
+ */
 void free_list(list_t *head)
 {
-    list_t *destroyed = head;
+	list_t *destroyed = head;
 
-    while (destroyed != NULL)
-    {
-        list_t *temp = destroyed;
-        destroyed = destroyed->next;
-        free(temp->str);
-        free(temp);
-    }
+	while (destroyed != NULL)
+	{
+	list_t *temp = destroyed;
+
+	destroyed = destroyed->next;
+	free(temp->str);
+	free(temp);
+	}
 }
