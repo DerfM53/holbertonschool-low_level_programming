203a913c9a43255f9d8f7b87489cbc84d90704e1
DerfM53 <frederic.mieuzet@gmail.com>
2024-07-31 20:08:02 +0000

    correction syntaxe files for betty

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -2,20 +2,26 @@
 #define _LISTS_H_
 #include <stddef.h>
 /**
- * struct dlistint_s - doubly linked list
- * @n: integer
- * @prev: points to the previous node
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: lenght of the string
  * @next: points to the next node
  *
- * Description: doubly linked list node structure
- * 
+ * Description: singly linked list node structure
  */
-typedef struct dlistint_s
+typedef struct list_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
-} dlistint_t;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
