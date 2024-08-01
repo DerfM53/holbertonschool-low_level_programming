7dae097763e22c6c4f3a8e34e50a5a8d63686305
DerfM53 <frederic.mieuzet@gmail.com>
2024-08-01 08:28:51 +0000

    mod file for betty

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -16,38 +16,36 @@
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *newnode;
-    list_t *last = *head;
+	list_t *new_node;
+	list_t *temp;
+	char *new_str;
+	int len;
 
-    newnode = malloc(sizeof(list_t));
-    if (newnode == NULL) /*Check if malloc failed*/
-    {
-        return (NULL);
-    }
+	new_str = strdup(str);
+	if (new_str == NULL)
+	return (NULL);
 
-    /*Duplicate the string and assign it to the new node*/
-    newnode->str = strdup(str);
-    if (newnode->str == NULL) /*Check if strdup failed*/
-    {
-        free(newnode); /*Free the allocated node if strdup fails*/
-        return (NULL);
-    }
+	for (len = 0; str[len]; len++);
 
-    /*Calculate the length of the string and assign it to the new node*/
-    newnode->len = strlen(newnode->str);
-    newnode->next = NULL;
-
-    if (*head == NULL)
-    {
-        *head = newnode;
-        return (newnode);
-    }
-    
-    while (last->next !=NULL)
-        {
-            last = last->next;
-        }
-    last->next = newnode;
-
-    return (newnode); /*Return the address of the new node*/
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		free(new_str);
+		return (NULL);
+	}
+	new_node->str = new_str;
+	new_node->len = len;
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+	temp = *head;
+	while (temp->next != NULL)
+	temp = temp->next;
+	temp->next = new_node;
+	}
+return (new_node);
 }
