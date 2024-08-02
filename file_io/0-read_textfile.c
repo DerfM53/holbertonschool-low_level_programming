ae39044907380f82db26276e4833f60a2683f369
DerfM53 <frederic.mieuzet@gmail.com>
2024-08-02 10:46:27 +0000

    correction file for betty

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -21,29 +21,34 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytesWritten; /*Nombre d'octets écrits*/
 
 	if (filename == NULL)
+	{
 		return (0);
-
+	}
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
 		return (0);
-
+	}
 	buffer = malloc(letters);
 	if (buffer == NULL)
+	{
 		close(fd);
 		return (0);
-
+	}
 	bytesRead = read(fd, buffer, letters);
 	if (bytesRead == -1)
+	{
 		free(buffer);
 		close(fd);
 		return (0);
-
+	}
 	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
 	if (bytesWritten == -1 || bytesWritten != bytesRead)
+	{
 		free(buffer);
 		close(fd);
 		return (0);
-
+	}
 	free(buffer);
 	close(fd);
 
