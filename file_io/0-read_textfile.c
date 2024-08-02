198876e61df78d69b9d423112b65456df78e3d2b
DerfM53 <frederic.mieuzet@gmail.com>
2024-08-02 10:40:11 +0000

    mod syntaxe of file for betty

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file and prints it to the
+ * POSIX standard output.
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
  *
@@ -12,45 +13,39 @@
  *         if filename is NULL, or if write fails or does not
  *         write the expected amount of bytes.
  */
-ssize_t read_textfile(const char *filename, size_t letters) 
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd; /* Descripteur de fichier*/
 	char *buffer; /*Tampon pour stocker les données lues*/
 	ssize_t bytesRead; /*Nombre d'octets lus*/
 	ssize_t bytesWritten; /*Nombre d'octets écrits*/
 
-	if (filename == NULL) 
-	{
+	if (filename == NULL)
 		return (0);
-	}
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1) {
+	if (fd == -1)
 		return (0);
-	}
 
 	buffer = malloc(letters);
-	if (buffer == NULL) {
+	if (buffer == NULL)
 		close(fd);
 		return (0);
-	}
 
 	bytesRead = read(fd, buffer, letters);
-	if (bytesRead == -1) {
+	if (bytesRead == -1)
 		free(buffer);
 		close(fd);
 		return (0);
-	}
 
 	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
-	if (bytesWritten == -1 || bytesWritten != bytesRead) {
+	if (bytesWritten == -1 || bytesWritten != bytesRead)
 		free(buffer);
 		close(fd);
 		return (0);
-	}
 
 	free(buffer);
 	close(fd);
-	
+
 	return (bytesWritten);
 }
